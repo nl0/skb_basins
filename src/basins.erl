0cc1dd762e8f173c036dafc75422faee8f073a43
nl_0 <nl.imbecile@gmail.com>
2012-08-15 00:51:23 +0600

    straitforward solution.

--- src/basins.erl
+++ src/basins.erl
@@ -1,5 +1,5 @@
 -module(basins).
--export([is_prime/1]).
+-export([is_prime/1, solve/1]).
 
 
 is_prime(N) when N =< 0 -> error;
@@ -9,4 +9,15 @@ is_prime(N) -> is_prime(N, 2, trunc(math:sqrt(N))).
 is_prime(_N, R, Sqrt) when R > Sqrt -> true;
 is_prime(N, R, _Sqrt) when N rem R == 0 -> false;
 is_prime(N, R, Sqrt) -> is_prime(N, R + 1, Sqrt).
+
+
+solve(Max) -> solve(2, Max, []).
+
+solve(N, Max, Results) when N > Max -> Results;
+solve(N, Max, Results) ->
+	NewResults = case is_prime(N) of
+		true -> [N|Results];
+		false -> Results
+	end,
+	solve(N + 1, Max, NewResults).
 
