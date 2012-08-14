26bce0e92f366726822766fe4ecbeb257dffeb65
nl_0 <nl.imbecile@gmail.com>
2012-08-15 00:34:25 +0600

    initial: Emakefile, is_prime.

---
+++ src/basins.erl
@@ -0,0 +1,12 @@
+-module(basins).
+-export([is_prime/1]).
+
+
+is_prime(N) when N =< 0 -> error;
+is_prime(N) when N =< 3 -> true;
+is_prime(N) -> is_prime(N, 2, trunc(math:sqrt(N))).
+
+is_prime(_N, R, Sqrt) when R > Sqrt -> true;
+is_prime(N, R, _Sqrt) when N rem R == 0 -> false;
+is_prime(N, R, Sqrt) -> is_prime(N, R + 1, Sqrt).
+
