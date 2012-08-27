6945a6b5047cc7f622125baa2e994b403ae7dbc4
nl_0 <nl.imbecile@gmail.com>
2012-08-28 01:55:39 +0600

    writing result to file

--- src/basins.erl
+++ src/basins.erl
@@ -9,7 +9,16 @@ start(Max, Name) ->
 	true = register(Name, Pool),
 	ok = gen_server:call(Pool, subscribe),
 	log:log("spawned pool ~p at node ~p, registered as ~p~n", [Pool, node(), Name]),
-	wait(Pool, Max).
+	{ok, File} = file:open("basins.txt", [write]),
+	PrimeHandler = fun(P, IsLast) ->
+		Fmt = case IsLast of
+			last -> "~p~n";
+			not_last -> "~p, "
+		end,
+		io:fwrite(File, Fmt, [P])
+	end,
+	wait(Pool, PrimeHandler, Max),
+	file:close(File).
 
 
 prime_test(Pid, N) ->
@@ -28,41 +37,43 @@ last_state(Last) ->
 	end.
 
 
-wait(Pool, Max) -> wait(Pool, Max, 0).
+wait(Pool, PrimeHandler, Max) -> wait(Pool, PrimeHandler, Max, 0).
 
-wait(Pool, 1, 0) ->
+wait(Pool, PrimeHandler, 2, 0) ->
+	PrimeHandler(2, last),
 	io:format("all done~n"),
 	exit(Pool, shutdown);
 
-wait(Pool, N, Wip) ->
-	io:format("~p (~p+~p) numbers to test~n", [N+Wip, Wip, N]),
+wait(Pool, PrimeHandler, N, Wip) ->
+	io:format("~p (~p+~p) numbers to test~n", [N+Wip-2, Wip, N-2]),
 	receive
 		{dpool_state, L, C, Q} ->
 			{LastL, LastC, LastQ} = last_state({L, C, Q}),
-			if N =< 1 ->
+			if N =< 2 ->
 				log:log("waiting for ~p workers to finish~n", [Wip]),
-				wait(Pool, N, Wip);
+				wait(Pool, PrimeHandler, N, Wip);
 			true ->
 				case LastL >= LastC of
 				true ->
 					log:log("dpool is overloaded: ~p/~p (~p) -> waiting~n", [LastL, LastC, LastQ]),
-					wait(Pool, N, Wip);
+					wait(Pool, PrimeHandler, N, Wip);
 				false ->
 					log:log("dpool load is ~p/~p (~p) -> running new worker (~p)~n", [LastL, LastC, LastQ, N]),
 					gen_server:call(Pool, {start, prime_test(self(), N)}),
-					wait(Pool, N-1, Wip+1)
+					wait(Pool, PrimeHandler, N-1, Wip+1)
 				end
 			end;
-		{dpool_worker, _} -> wait(Pool, N, Wip);
+		{dpool_worker, _} -> wait(Pool, PrimeHandler, N, Wip);
 		{prime, P, true} ->
 			log:log("~p is prime~n", [P]),
-			wait(Pool, N, Wip-1);
+			PrimeHandler(P, not_last),
+			wait(Pool, PrimeHandler, N, Wip-1);
 		{prime, P, false} ->
 			log:log("~p is not prime~n", [P]),
-			wait(Pool, N, Wip-1);
+			wait(Pool, PrimeHandler, N, Wip-1);
 		Unexpected ->
 			log:log("unexpected msg: ~p~n", [Unexpected]),
-			wait(Pool, N, Wip)
+			wait(Pool, PrimeHandler, N, Wip)
 	end.
 
 
